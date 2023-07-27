a156cc42f60e8dcff1b253cf922d0d917d67e705
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-07-27 21:00:10 +0400

    Add files via upload

---
+++ Assets/Scripts/Bonus/BonusHpUp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+// Бонус добавления жизней игроку
+public class BonusHpUp : Bonus
+{
+    [SerializeField] private int hpAdd;
+
+    protected override void GiveBonus()
+    {
+        unit.GetComponentInChildren<Character>().HealthPoints += hpAdd;
+        Destroy(gameObject);
+    }
+}
