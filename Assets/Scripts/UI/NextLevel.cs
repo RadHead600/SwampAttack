108f981c288f2a675df10458a464d733efd5377e
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-08-03 19:12:21 +0400

    Update NextLevel.cs

--- Assets/Scripts/UI/NextLevel.cs
+++ Assets/Scripts/UI/NextLevel.cs
@@ -1,12 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    // Функция старта следующего уровня
     public void OpenNextLevel()
     {
         SaveParameters.levelActive++;
+
         if (SceneManager.sceneCountInBuildSettings > SaveParameters.levelActive)
         {
             SceneManager.LoadScene(SaveParameters.levelActive);
