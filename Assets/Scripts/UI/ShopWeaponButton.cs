8921a9a5a63ec33cf4df5c0e25ce554b83361093
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-08-04 12:23:34 +0400

    Update ShopWeaponButton.cs

--- Assets/Scripts/UI/ShopWeaponButton.cs
+++ Assets/Scripts/UI/ShopWeaponButton.cs
@@ -4,13 +4,13 @@ using UnityEngine.UI;
 
 public class ShopWeaponButton : MonoBehaviour
 { 
-    [SerializeField] private Button button;
-    [SerializeField] private TextMeshProUGUI costText;
-    [SerializeField] private TextMeshProUGUI nameText;
-    [SerializeField] private Image weaponImage;
+    [SerializeField] private Button _button;
+    [SerializeField] private TextMeshProUGUI _costText;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private Image _weaponImage;
 
-    public Button Button => button;
-    public TextMeshProUGUI CostText => costText;
-    public TextMeshProUGUI NameText => nameText;
-    public Image WeaponImage => weaponImage;
+    public Button Button => _button;
+    public TextMeshProUGUI CostText => _costText;
+    public TextMeshProUGUI NameText => _nameText;
+    public Image WeaponImage => _weaponImage;
 }
